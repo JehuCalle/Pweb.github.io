ea697bda104612259789d98d46868190151734e1
JehuCalle <josiasandres56@gmail.com>
2022-10-20 12:54:38 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ formulario.addEventListener("submit",(e)=>{
         agregarUsuario(rut,pass);
         alerta.classList.add("d-none");
         console.log(usuarios)
-        setTimeout(()=> location.href="./MisDatos.html",2000);
+        setTimeout(()=> location.href="./Misdatos.html",2000);
         return;
     }
 });
